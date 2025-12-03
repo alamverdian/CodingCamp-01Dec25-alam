db087d7f5b2c62512ad9147d7011de83ff3e89b0
Alam Verdian Syah Daulay <alamverdiansyahdaulay@gmail.com>
2025-12-03 14:20:59 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.getElementById("msgForm").addEventListener("submit", function(event){
+  event.preventDefault();
+
+  // ambil value form
+  const nama = document.getElementById("nama").value;
+  const tanggal = document.getElementById("tanggalLahir").value;
+  const jk = document.querySelector("input[name='jk']:checked").value;
+  const pesan = document.getElementById("pesan").value;
+
+  // current time
+  const now = new Date().toLocaleString();
+
+  // output
+  document.getElementById("waktu").innerText = now;
+  document.getElementById("outNama").innerText = nama;
+  document.getElementById("outTanggal").innerText = tanggal;
+  document.getElementById("outJK").innerText = jk;
+  document.getElementById("outPesan").innerText = pesan;
+});
